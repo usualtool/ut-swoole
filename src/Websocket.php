d6911bf733ff538249891154439a124ef20c3735
usualtool <52517593+usualtool@users.noreply.github.com>
2023-02-19 22:06:35 +0800

    Update Websocket.php

--- src/Websocket.php
+++ src/Websocket.php
@@ -2,9 +2,10 @@
 namespace usualtool\Swoole;
 class Websocket{
     private $server = null;
-    public function __construct($host,$port){
+    public function __construct($host,$port,$type='only'){
         $this->host = $host;
         $this->port = $port;
+        $this->type = $type;
         $this->server = new \Swoole\Websocket\Server($this->host,$this->port);
     }
     public function Run(){
@@ -15,10 +16,28 @@ class Websocket{
         $this->server->start();
     }
     public function OnOpen($server,$request){
-        $server->push($request->fd,"hello,The ".$request->fd.".");
+        //$server->push($request->fd,"hello,The ".$request->fd.".");
     }
     public function OnMessage($server,$frame){
-        $server->push($frame->fd,$frame->data);
+        //对单连接
+        if(empty($this->type) || $this->type=="only"){
+            $server->push($frame->fd,$frame->data);
+        //对多连接
+        }elseif($this->type=="more"){
+            //数据格式{"type":"speak","msg":{"name":"昵称","speak":"内容"}}
+            $data=json_decode($frame->data, true);
+            if($data["type"]=="login"){
+                $send_msg = "进入了房间。";
+            }elseif($data["type"]=="logout"){
+                $send_msg = "退出了房间。";
+            }else{
+                $send_msg = ":".$data["msg"]["speak"];
+            }
+            foreach($server->connections as $fd){
+                $name=$data['msg']['name'];
+                $server->push($fd,$name.$send_msg);
+            }
+        }
     }
     public function OnRequest($request,$response){
         foreach ($this->server->connections as $fd) {
@@ -28,6 +47,6 @@ class Websocket{
         }
     }
     public function OnClose($server, $fd){
-        echo "The ".$fd." closed.\r\n";
+        //echo "The ".$fd." closed.\r\n";
     }
 }
